f567e5b4f54ec42cc9f4b29f1a9dbc52a1c27685
Severin <sevefriede@gmail.com>
2017-11-16 20:23:23 +0100

    added now env settings

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || app.get('port');
 const host = process.env.NOW_URL || app.get('host');
 const server = app.listen(port);
 
-server.on('listening', () =>
-  logger.info('Feathers application started on %s:%d', host, port)
-);
+server.on('listening', () => {
+  logger.info('Feathers application started on %s:%d', host, port);
+});
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
